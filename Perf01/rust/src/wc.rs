d0284d5c8ab087193b00067969e9d3db053e7d11
WooYooWaan <wooyoowaan@gmail.com>
2018-09-12 09:02:44 +0900

    add macOS result

--- Perf01/rust/src/wc.rs
+++ Perf01/rust/src/wc.rs
@@ -1,37 +1,60 @@
 use std::env;
+use std::vec::Vec;
 use std::result::Result;
 use std::fs::File;
 use std::io::Read;
 use std::io::Seek;
 use std::io::SeekFrom;
+use std::sync::mpsc;
+use std::thread;
+
+fn countup(s: &str) -> (i32, i32, i32) {
+    println!("{}", s);
+    (0, 0, 0)
+}
 
 
-//#[warn(unused_parens)] 
 fn word_count(file: &String) -> i32  {
     let mut f = File::open(file).unwrap();
 
     let size: i64 = 10;
-    let mut buffer = [0; 10];
     let mut done = false;
+    let mut i: i64 = 0;
+    let (tx, rx) = mpsc::channel();
+    let mut buffer = [0; 10];
 
     while !done {
-        let sz: usize = match f.read(&mut buffer[..]) {
+        match f.read(&mut buffer[..]) {
             Result::Ok(sz) => {
                 let s = std::str::from_utf8(&buffer).unwrap();
-                println!("{}", s);
-                sz;
+                let tx = tx.clone();
+                thread::spawn(move || {
+                    tx.send(countup(s))
+                });
+                done = sz != 10;
             },
             Result::Err(_) => panic!("failed read file"),
         }
 
-        if (sz != 10) {
-            done = false;
-        } else {
-            f.seek(SeekFrom::Current(size));
+        if (!done) {
+            match f.seek(SeekFrom::Current(size * i)) {
+                Result::Ok(_) => i += 1,
+                Result::Err(_) => panic!("failed read file"),
+            }
         }
     }
 
+    let mut data: Vec<(i32,i32,i32)> = Vec::with_capacity(i as usize);
+    for _ in 0..i {
+        let rs = rx.recv();
+        match rs {
+            Ok(wc) => data.push(wc),
+            Err(e) => println!("{}", e),
+        }
+    }
 
+    let cnt = data.iter().fold((0,0,0), |total,x| (total.0 + x.0, total.1 + x.1, total.2 + x.2));
+    println!("{:?}", cnt);
     return 0;
 }
 
